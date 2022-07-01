d361ca6bcb6db8061b6990994014e648c931d926
jorda <jordan.rachid@gmail.com>
2022-06-30 21:46:22 -0300

    first commit

---
+++ src/java_bank/Cliente.java
@@ -0,0 +1,14 @@
+package java_bank;
+
+public class Cliente {
+
+	private String nome;
+	
+	Cliente(String nome){
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+
+}
---
+++ src/java_bank/Main.java
@@ -0,0 +1,8 @@
+package java_bank;
+
+public class Main {
+	public static void main(String[] args) {
+	Cliente Rachid = new Cliente("rachid Bastos");
+	System.out.print(Rachid.getNome());
+}
+}
